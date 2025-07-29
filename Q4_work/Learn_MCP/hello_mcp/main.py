1b13415f99c824b9f9ec55e4329054b6a8529fa1
Danii9790 <daniyalashraf9790@gmail.com>
2025-07-29 16:01:14 +0500

    Update

--- Q4_work/Learn_MCP/hello_mcp/main.py
+++ Q4_work/Learn_MCP/hello_mcp/main.py
@@ -2,4 +2,10 @@ from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP(name="Hello-mcp", stateless_http = True)
 
+@mcp.tool(name="online_researcher",description="Seach the Web for information")
+def seach_online(query:str) ->str :
+    return f"Results for {query}...."
+
+
+
 mcp_app =mcp.streamable_http_app()
