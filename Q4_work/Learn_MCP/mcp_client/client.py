c6f415a562382b9c7ca0cebd973dc284f4db3f5b
Danii9790 <daniyalashraf9790@gmail.com>
2025-08-27 23:17:35 +0500

    Update

--- Q4_work/Learn_MCP/mcp_client/client.py
+++ Q4_work/Learn_MCP/mcp_client/client.py
@@ -1,54 +1,5 @@
-# from ast import arg
-# import asyncio
-# from inspect import stack
-# from mcp import ClientSession
-# from mcp.client.streamable_http import streamablehttp_client
-# from contextlib import AsyncExitStack
-# import rich
-# class MCPClient:
-#     def __init__(self,url):
-#         self.url = url
-#         self.stack = AsyncExitStack() 
-#         self._sess = None
-    
-#     async def list_tools(self):
-#         async with self.session as session:
-#             response = (await session.list_tools()).tools
-#             return response
-#     async def __aenter__(self): 
-#         # Pizza Shop --> Counter ka banda jo Customer sy deal kr raha hn (Connection)
-#         read,write,_ = await self.stack.enter_async_context(
-#             streamablehttp_client(self.url)
-#         )
-#         # Backend jaha Pizza bana raha hn (All backend Done by )
-#         self._sess = await self.stack.enter_async_context(
-#             ClientSession(read,write)
-#         )
-#         await self._sess.initialize()
-#         return self
-#     async def __aexit__(self,*args):
-#         await self.stack.aclose()
-
-#     async def list_tools(self):
-#         return (await self._sess.list_tools()).tools
-
-    
-#     async def call_tools(self,tool_name,*args,**kwards):
-#         return await self._sess.call_tool(tool_name,*args,**kwards)
-        
-
-# async def main():
-#     async with MCPClient("http://localhost:8000/mcp") as client:
-#         tools = await client.list_tools()
-#         rich.print(tools,"tools")
-
-#         for tool in tools:
-#             result = await client.call_tools(tool.name,{"name":"Daniyal"})
-#             rich.print(f"Tool '{tool.name}' output:", result)
-
-# asyncio.run(main())
-
 import asyncio
+import json
 from mcp import ClientSession , types ,Resource
 from mcp.client.streamable_http import streamablehttp_client
 from contextlib import AsyncExitStack
@@ -81,24 +32,40 @@ class MCPClient:
     async def list_resources(self) -> list[types.Resource]:
         result : types.ListResourcesResult = await self._sess.list_resources()
         return result.resources
+        
+    async def list_resource_templates(self) -> list[types.ResourceTemplate]:
+        assert self._sess, "Session not available."
+        result: types.ListResourceTemplatesResult = await self._sess.list_resource_templates()
+        # print("LIST RESOURCE TEMPLATES", result.__dict__)
+        return result.resourceTemplates
 
-    async def  read_resources(self , uri : str) -> types.ReadResourceResult:
-        assert self._sess , "Session is not available"
-        _url  = AnyUrl(uri)
-        result = await self._sess.read_resource(_url)
-        # rich.print("Read resouce dict",result.__dict__)
+    async def read_resources(self, uri: str) -> types.ReadResourceResult:
+        assert self._sess, "Session not available."
+        result = await self._sess.read_resource(AnyUrl(uri))
+        # print("READ RECOURSES DICT", result.__dict__)
         resource = result.contents[0]
-        return resource
+        if isinstance(resource, types.TextResourceContents):
+            if resource.mimeType == "application/json":
+                try:
+                    return json.loads(resource.text)
+                except json.JSONDecodeError as e:
+                    print(f"Error decoding JSON: {e}")
+        return resource.text
+
 
 async def main():
     async with MCPClient("http://localhost:8000/mcp") as client:
-        tools = await client.list_tools()
-        rich.print(tools, "tools")
+        # tools = await client.list_tools()
+        # rich.print(tools, "tools")
 
-        resources = await client.list_resources()
-        rich.print(resources,"resources")
+        # resources = await client.list_resources()
+        # rich.print(resources,"resources")
 
-        resources = await client.read_resources("docs://documents")
-        rich.print(resources,"resources")
+        # resources = await client.read_resources("docs://documents")
+        # rich.print(resources,"resources")
+        template = await client.list_resource_templates()
+        intro_uri = template[0].uriTemplate.replace("{doc_name}","intro")
+        data = await client.read_resources(intro_uri)
+        print("Intro Document :",data)
 
 asyncio.run(main())
--- Q4_work/Learn_MCP/mcp_client/server.py
+++ Q4_work/Learn_MCP/mcp_client/server.py
@@ -13,5 +13,14 @@ def list_docs():
     return list(docs.keys())
 
 print("list_docs",list_docs())
+
+@mcp.resource("docs://documents/{doc_name}",
+                mime_type="application/json")
+def read_doc(doc_name: str):
+    """Read a specific document."""
+    if doc_name in docs:
+        return {"name": doc_name, "content": docs[doc_name]}
+    else:
+        raise mcp.ResourceNotFound(f"Document '{doc_name}' not found.")
     
 mcp_server = mcp.streamable_http_app()
